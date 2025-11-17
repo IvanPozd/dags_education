839b22932c1aa52b5405e8b69d6471a6843babec
Ivan Pozdeev <ipozdeev@neoflex.ru>
2025-11-17 12:29:35 +0300

    ADD: read task

--- education/download_rocket_launches.py
+++ education/download_rocket_launches.py
@@ -1,11 +1,28 @@
+import json
+
 from airflow.providers.standard.operators.bash import BashOperator
+from airflow.providers.standard.operators.python import PythonOperator
 from airflow.sdk import DAG
+
+
+def read_launches_file():
+    with open("/tmp/launches.json") as json_file:
+        content = json.load(json_file)
+        print(content)
+
 
 with DAG(
     dag_id="download_rocket_launches",
     schedule="@hourly",
 ) as dag:
-    BashOperator(
-        task_id="download_launches",
+    download = BashOperator(
+        task_id="download_launches_task",
         bash_command="curl -o /tmp/launches.json -L 'https://ll.thespacedevs.com/2.0.0/launch/upcoming'",
     )
+
+    read = PythonOperator(
+        task_id="read_launches_file_task",
+        python_callable=read_launches_file,
+    )
+
+    download >> read
